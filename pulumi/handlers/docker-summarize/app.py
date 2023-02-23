a99d47c0a37f82cba90fa1add27f2a275ae2bd46
ankush <unkush-garg@github.com>
2023-02-23 17:32:38 +0530

    Add speech synthesis function call to summarize api

--- pulumi/handlers/docker-summarize/app.py
+++ pulumi/handlers/docker-summarize/app.py
@@ -1,16 +1,15 @@
 import json
 import base64
+import os
+import requests
 from summarize import summarize
 from uploader_service import upload
 from get_articles import get_articles
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def handler(event, context):
-    # body = event.get('body')
-    # if body is None or body == "":
-    #     return "Body is empty. Expected JSON object.", 422
-
-    # body = json.loads(base64.b64decode(body))
-
     articles = get_articles(number_of_stories=10)
     # For each article, generate summaries of full articles
     summarized_articles = []
@@ -28,9 +27,21 @@ def handler(event, context):
     # Upload the entire article including summaries
     upload(summarized_articles)
 
-    # Emit a message that the lambda finished it's job
-    
+    # After upload completes, send a post content to an api
+    for article in articles:
+        content = article['content']
+        
+        # Only take 6000 characters to summarize
+        if len(content) > 3000:
+            content = content[:3000]
 
+        response = requests.post(
+            url=os.getenv("API_TEXT_TO_SPEECH"),
+            json={
+                "text": content,
+                "textId": article['id'],
+                "voiceId": "Joanna"
+                })
     return {
         "status": 200,
         "description": "Complete."
